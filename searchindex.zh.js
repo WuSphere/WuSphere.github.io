83c01406a65d2e4515b6c7146a27e8f1d931747d
WuSphere <WuSphere@users.noreply.github.com>
2025-03-26 04:01:54 +0000

    deploy: 433120f833e975338f139504881d74e9c6d2d60d

---
+++ searchindex.zh.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "我的网站",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "类别",
+    "uri": "/zh/categories/index.html"
+  },
+  {
+    "breadcrumb": "我的网站",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "标签",
+    "uri": "/zh/tags/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "我的网站",
+    "uri": "/zh/index.html"
+  }
+]
